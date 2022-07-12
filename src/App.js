ef2379f2ee1ec8332dcdaf8fc1fc3106f5581662
zamirbekovsamagan <zamirbekovsamagan1@gmail.com>
2022-07-13 00:30:49 +0300

    custom

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import TableList from './components/UI/TableList'
+
+function App() {
+   return (
+      <div>
+         Hello <TableList edit />
+      </div>
+   )
+}
+
+export default App
